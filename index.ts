12c7e744213bd31b0bf6043e2bacc182139531bc
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-23 14:24:47 -0700

    Converted the packageInfo export to a default export

--- index.ts
+++ index.ts
@@ -4,18 +4,18 @@ import packageJSON from './package.json' with {type: 'json'};
 import * as tools from "./tools.ts";
 import WikipediaService, {WikipediaConfigSchema} from "./WikipediaService.ts";
 
-export const packageInfo: TokenRingPackage = {
+export default {
   name: packageJSON.name,
   version: packageJSON.version,
   description: packageJSON.description,
   install(agentTeam: AgentTeam) {
-    agentTeam.addTools(packageInfo, tools);
+    agentTeam.addTools(packageJSON.name, tools);
     const config = agentTeam.getConfigSlice('wikipedia', WikipediaConfigSchema.optional());
     if (config) {
       agentTeam.addServices(new WikipediaService(config));
     }
   },
-};
+} as TokenRingPackage;
 
 
 export {default as WikipediaService} from "./WikipediaService.ts";
