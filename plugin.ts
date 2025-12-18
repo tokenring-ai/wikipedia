2e7d808500dc580589fed30a1b4b947bd53f15b7
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:39 -0800

    Extract and modularize plugin logic into `plugin.ts`

---
+++ plugin.ts
@@ -0,0 +1,23 @@
+import TokenRingApp from "@tokenring-ai/app";
+import {ChatService} from "@tokenring-ai/chat";
+import {TokenRingPlugin} from "@tokenring-ai/app";
+import packageJSON from './package.json' with {type: 'json'};
+
+import tools from "./tools.ts";
+import WikipediaService, {WikipediaConfigSchema} from "./WikipediaService.ts";
+
+
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(app: TokenRingApp) {
+    app.waitForService(ChatService, chatService =>
+      chatService.addTools(packageJSON.name, tools)
+    );
+    const config = app.getConfigSlice('wikipedia', WikipediaConfigSchema.optional());
+    if (config) {
+      app.addServices(new WikipediaService(config));
+    }
+  },
+} as TokenRingPlugin;
