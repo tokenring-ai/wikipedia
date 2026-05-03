d3d08c5223551053052a609f4b680b568f6d55c6
Mark Dierolf <mdierolf@financialcontent.com>
2026-05-02 21:33:17 -0700

    Organize Wikipedia tools exports; add individual and grouped exports

--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
 export { default as WikipediaService } from "./WikipediaService.ts";
+export type { ParsedWikipediaConfig, WikipediaSearchOptions } from "./WikipediaService.ts";
+export { WikipediaConfigSchema } from "./WikipediaService.ts";
--- tools.ts
+++ tools.ts
@@ -1,4 +1,15 @@
 import getPage from "./tools/getPage.ts";
 import search from "./tools/search.ts";
 
+// Array export for spreading into addTools()
 export default [search, getPage];
+
+// Named exports for individual tool access
+export { search as wikipedia_search };
+export { getPage as wikipedia_getPage };
+
+// Object export for tools collection
+export const tools = {
+  search,
+  getPage,
+};
